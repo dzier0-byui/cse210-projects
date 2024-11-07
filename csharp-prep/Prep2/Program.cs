87bae6b5f1023488bca0a1992737ae31ef2a65c3
dier0-byui <dzi24004@byui.edu>
2024-11-06 21:32:56 -0700

    completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        if (grade >= 90){
+            letter = "A";
+        }
+        else if (grade >= 80){
+            letter = "B";
+        }
+        else if (grade >= 70){
+            letter = "C";
+        }
+        else if (grade >= 60){
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+        if (lastDigit >= 7 && letter != "A" && letter != "F"){
+            letter += "+";
+        }
+        else if (lastDigit < 3 && letter != "F"){
+            letter += "-";
+        }
+
+        Console.WriteLine($"Your grade is {letter} at {grade}%");
+
+        if (grade >= 70){
+            Console.WriteLine("Congrats you passed!!");
+        }
+        else{
+            Console.WriteLine("You did not pass");
+        }
     }
 }
