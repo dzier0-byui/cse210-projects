c0f1d4588b6503a7955a9b34ad61e1e1e0fa1a2f
dier0-byui <dzi24004@byui.edu>
2024-11-07 22:33:34 -0700

    completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess;
+        int numberOfGuesses = 0;
+
+        do{
+            numberOfGuesses++;
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number){
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number){
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("you got it!!");
+            }
+
+        } while (guess != number);
+
+        string guessWord = (numberOfGuesses > 1) ? "guesses" : "guess";
+        Console.WriteLine($"It took you {numberOfGuesses} {guessWord}");
     }
 }
