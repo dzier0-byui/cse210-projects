c94e5e235d5df844436e2b95d20458c9e30170a1
dier0-byui <dzi24004@byui.edu>
2024-11-12 22:48:14 -0700

    finished learning assignment for week 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Poulton Insurance";
+        job2._jobTitle = "Data Analyst";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Cody Dzierzon";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.DisplayResume();
     }
 }
